7eeed795ddfdaa7e2f9cae50f815f5f5b22ff9d2
AhmedDiaa69 <diaaa625@gmail.com>
2025-06-12 00:47:43 +0300

    V1.2

--- To-Do2/src/Input.jsx
+++ To-Do2/src/Input.jsx
@@ -8,7 +8,6 @@ function Input() {
   const containerRef = useRef(null);
 
   let [tasks, setTasks] = useState([]);
-  let [_, setShowWarning] = useState(false);
 
   function AddTask() {
     let newDate = document.getElementById("date").value;
@@ -16,7 +15,6 @@ function Input() {
     let nowDate = new Date().toISOString().split("T")[0];
 
     if (newTask.trim() == "" || new Date(newDate) < new Date(nowDate)) {
-      setShowWarning(true);
       open();
       return;
     }
@@ -36,7 +34,7 @@ function Input() {
     let thisTask = tasks[index];
     let previousTask = tasks[index - 1];
     if (!previousTask) {
-      window.alert("you can't do that");
+      openTask();
       return;
     }
 
@@ -51,7 +49,7 @@ function Input() {
     let thisTask = tasks[index];
     let nextTask = tasks[index + 1];
     if (!nextTask) {
-      window.alert("you can't do that");
+      openTask();
       return;
     }
 
@@ -62,6 +60,7 @@ function Input() {
   }
 
   let [isOpen, setIsOpen] = useState(false);
+  let [isTaskOpen, setIsTaskOpen] = useState(false);
 
   function open() {
     setIsOpen(true);
@@ -69,6 +68,14 @@ function Input() {
 
   function close() {
     setIsOpen(false);
+  }
+
+  function openTask() {
+    setIsTaskOpen(true);
+  }
+
+  function closeTask() {
+    setIsTaskOpen(false);
   }
 
   return (
@@ -103,6 +110,45 @@ function Input() {
                   onKeyUp={(e) => {
                     if (e.key === "Enter") {
                       close();
+                    }
+                  }}
+                >
+                  Got it, thanks!
+                </Button>
+              </div>
+            </DialogPanel>
+          </div>
+        </div>
+      </Dialog>
+            <Dialog
+        open={isTaskOpen}
+        as="div"
+        className="relative z-10 focus:outline-none"
+        onClose={closeTask}
+      >
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4">
+            <DialogPanel
+              transition
+              className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0"
+            >
+              <DialogTitle
+                as="h3"
+                className="text-base/7 font-medium text-white"
+              >
+                Something is wrong
+              </DialogTitle>
+              <p className="mt-2 text-sm/6 text-white/50">
+                Your task is already on the top or the bottom 😊
+              </p>
+              <div className="mt-4">
+                <Button
+                  className="inline-flex items-center gap-2 rounded-lg bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700"
+                  onClick={closeTask}
+                  autoFocus
+                  onKeyUp={(e) => {
+                    if (e.key === "Enter") {
+                      closeTask();
                     }
                   }}
                 >
