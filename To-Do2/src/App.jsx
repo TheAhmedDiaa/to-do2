cf92a5c204092fe18821a0d0a7a020b6992ec79b
AhmedDiaa69 <diaaa625@gmail.com>
2025-06-11 23:58:20 +0300

    V1.1

--- To-Do2/src/App.jsx
+++ To-Do2/src/App.jsx
@@ -10,11 +10,11 @@ function App() {
             <SplitText
               text="Do-Me App 😝"
               className="text-5xl font-semibold text-center eader"
-              delay={150}
+              delay={400}
               duration={1}
               ease="power3.out"
               splitType="words"
-              from={{ opacity: 0, y: 40 }}
+              from={{ opacity: 0, y: 1000 }}
               to={{ opacity: 1, y: 0 }}
               threshold={0.1}
               rootMargin="-100px"
