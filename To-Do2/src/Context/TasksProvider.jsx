2f50e6a1d9220016b95a90f843c08fd1a15f5f22
AhmedDiaa69 <diaaa625@gmail.com>
2025-10-19 00:41:53 +0300

    Disabling the warnings

--- To-Do2/src/Context/TasksProvider.jsx
+++ To-Do2/src/Context/TasksProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState, useEffect, useContext } from "react";
 import { DialogContext } from "./DialogProvider.jsx";
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const TasksContext = createContext(null);
 
 export default function TasksProvider({ children }) {
